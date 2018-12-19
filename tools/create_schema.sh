335fc1f85655a0dc0852ff03d6c38e68c9d8c704
Erik Kemperman <erikkemperman@gmail.com>
2018-12-19 09:12:27 +0100

    Missing comma

--- tools/create_schema.sh
+++ tools/create_schema.sh
@@ -401,7 +401,7 @@ CREATE TABLE tls803_legal_event_code (
   event_descr varchar(250) NOT NULL DEFAULT '',
   event_descr_orig varchar(250) NOT NULL DEFAULT '',
   event_category_code char(1) NOT NULL DEFAULT '',
-  event_category_title varchar(100) NOT NULL DEFAULT ''
+  event_category_title varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (event_auth, event_code)
 ) ENGINE=${ENGINE} $ROW_FORMAT DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci  AVG_ROW_LENGTH=100;
 
